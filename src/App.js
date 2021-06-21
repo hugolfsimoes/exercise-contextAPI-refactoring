74981b28d9a890375d3a2ee8a6ae10af67f73550
Hugo Simoes <hugolfsimoes@gmail.com>
2021-06-21 18:24:41 -0300

    feito tarefa 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import TrafficSignal from './TrafficSignal';
 import './App.css';
 import Cars from './Cars';
 
 function App() {
   return (
-    <div className="container">
-      <Cars />
-      <TrafficSignal />
-    </div>
+    <Cars />
   );
 }
 
